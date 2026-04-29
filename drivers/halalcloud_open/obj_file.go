91153ae9e8c8556ddad11d5e37588c3e06310a74
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-04-30 02:54:13 +0800

    Update obj_file.go

--- drivers/halalcloud_open/obj_file.go
+++ drivers/halalcloud_open/obj_file.go
@@ -3,8 +3,8 @@ package halalcloudopen
 import (
 	"time"
 
-	"github.com/OpenListTeam/OpenList/v4/internal/model"
-	"github.com/OpenListTeam/OpenList/v4/pkg/utils"
+	"github.com/alist-org/alist/v3/internal/model"
+	"github.com/alist-org/alist/v3/pkg/utils"
 	sdkUserFile "github.com/halalcloud/golang-sdk-lite/halalcloud/services/userfile"
 )
 
