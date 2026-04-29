071e6597fe044b850f8cef15b42ad1ad0f1a954d
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-04-30 02:49:59 +0800

    Update driver_get_link.go

--- drivers/halalcloud_open/driver_get_link.go
+++ drivers/halalcloud_open/driver_get_link.go
@@ -7,9 +7,9 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/OpenListTeam/OpenList/v4/internal/model"
-	"github.com/OpenListTeam/OpenList/v4/internal/stream"
-	"github.com/OpenListTeam/OpenList/v4/pkg/http_range"
+	"github.com/alist-org/alist/v3/internal/model"
+	"github.com/alist-org/alist/v3/internal/stream"
+	"github.com/alist-org/alist/v3/pkg/http_range"
 	sdkUserFile "github.com/halalcloud/golang-sdk-lite/halalcloud/services/userfile"
 	"github.com/rclone/rclone/lib/readers"
 )
