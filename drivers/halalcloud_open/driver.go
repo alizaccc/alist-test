6b77c82af0147241e8527dabf9ad73b78f71ba5d
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-04-30 02:46:53 +0800

    Update driver.go

--- drivers/halalcloud_open/driver.go
+++ drivers/halalcloud_open/driver.go
@@ -1,8 +1,8 @@
 package halalcloudopen
 
 import (
-	"github.com/OpenListTeam/OpenList/v4/internal/driver"
-	"github.com/OpenListTeam/OpenList/v4/internal/model"
+	"github.com/alist-org/alist/v3/internal/driver"
+	"github.com/alist-org/alist/v3/internal/model"
 	sdkClient "github.com/halalcloud/golang-sdk-lite/halalcloud/apiclient"
 	sdkUser "github.com/halalcloud/golang-sdk-lite/halalcloud/services/user"
 	sdkUserFile "github.com/halalcloud/golang-sdk-lite/halalcloud/services/userfile"
