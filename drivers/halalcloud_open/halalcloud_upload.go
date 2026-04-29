3b99d7d1467ed7122a4fa1d7aa3bdfcc153a0d35
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-04-30 02:52:48 +0800

    Update halalcloud_upload.go

--- drivers/halalcloud_open/halalcloud_upload.go
+++ drivers/halalcloud_open/halalcloud_upload.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/OpenListTeam/OpenList/v4/internal/driver"
-	"github.com/OpenListTeam/OpenList/v4/internal/model"
+	"github.com/alist-org/alist/v3/internal/driver"
+	"github.com/alist-org/alist/v3/internal/model"
 	sdkUserFile "github.com/halalcloud/golang-sdk-lite/halalcloud/services/userfile"
 	"github.com/ipfs/go-cid"
 	log "github.com/sirupsen/logrus"
