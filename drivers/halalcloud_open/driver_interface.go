73b4fc6d21c6d96a83edf3463ec6b9c95d4bfc68
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-05-01 10:49:44 +0800

    Update driver_interface.go

--- drivers/halalcloud_open/driver_interface.go
+++ drivers/halalcloud_open/driver_interface.go
@@ -43,10 +43,6 @@ func (d *HalalCloudOpen) Put(ctx context.Context, dstDir model.Obj, stream model
 	return d.put(ctx, dstDir, stream, up)
 }
 
-func (d *HalalCloudOpen) GetDetails(ctx context.Context) (*model.StorageDetails, error) {
-	return d.details(ctx)
-}
-
 func (d *HalalCloudOpen) Remove(ctx context.Context, obj model.Obj) error {
 	return d.remove(ctx, obj)
 }
