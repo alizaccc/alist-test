8008d7f55186d97b277db125dc6a465b0a8b8d69
alizaccc <130232421+alizaccc@users.noreply.github.com>
2026-05-01 09:32:43 +0800

    Update all.go

--- drivers/all.go
+++ drivers/all.go
@@ -44,7 +44,7 @@ import (
 	_ "github.com/alist-org/alist/v3/drivers/google_photo"
 	_ "github.com/alist-org/alist/v3/drivers/guangyapan"
 	_ "github.com/alist-org/alist/v3/drivers/halalcloud"
-	_ "github.com/alist-org/alist/v3/drivers/halalcloud_open"
+	_ "github.com/alizaccc/alist-test/drivers/halalcloud_open"
 	_ "github.com/alist-org/alist/v3/drivers/ilanzou"
 	_ "github.com/alist-org/alist/v3/drivers/ipfs_api"
 	_ "github.com/alist-org/alist/v3/drivers/kodbox"
